b83090093009915fca9f0db3fe1484ad540527b1
Pierre Meunier <pierre.meunier.etu@univ-lille.fr>
2026-02-28 08:28:50 +0100

    :hammer: amelioration log restart.sh

--- script/restart.sh
+++ script/restart.sh
@@ -1,15 +1,26 @@
-
 #!/bin/bash
 
-# A script than restart all docker container stopped
+# A script that restart all the docker containers that were stopped
+# You will have to update LOG_FILE and SUMMARY_FILE to matching the desired location
+# LOG_FILE will contains one message at each start + when a container is started
+# SUMMARY_FILE will only contained messages about container started
+
+# Warning:  Use space before and after [ and ] when using a condition.
+# Else the syntax is incorrect
+
+LOG_FILE=/root/scripts/log.txt
+SUMMARY_FILE=/root/scripts/summary.txt
+
+echo $(date) : "Script restart.sh launched"
+echo $(date) : "Script restart.sh launched"  1>>$LOG_FILE 2>>$LOG_FILE
 
 for container in $(docker container ps -a -q); do
 
     if [ $(docker container ps -q | grep -e $container | wc -l) == 0 ]
     then
-        echo "Script restart.sh at" $(date) ": restarting container \"$container\""
-        echo "Script restart.sh at" $(date) ": restarting container \"$container\"" 1>> log.txt 2>> log.txt
-        docker container start $container 
+        echo $(date) ": restarting container \"$container\""
+        echo $(date) ": restarting container \"$container\"" 1>> $LOG_FILE 2>> $LOG_FILE
+        echo $(date) ": restarting container \"$container\"" 1>> $SUMMARY_FILE 2>> $SUMMARY_FILE
+        docker container start $container
     fi
 done
-
